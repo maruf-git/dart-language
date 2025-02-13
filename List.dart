8009b66327cdd8ca8fb1c197e093b6b7f93346ff
orion <munna.csehstu@gmail.com>
2025-02-13 13:09:56 +0600

    intro to oop

---
+++ List.dart
@@ -0,0 +1,9 @@
+void main(){
+  List<int> results =[10,20,30,40];
+  print(results);
+
+  var age = [10,20,30,40]; // var means List<int>
+  print(age);
+  print("check age data type is List : ${age is List<double>}");
+  print ("data type of age variable ${age.runtimeType}");
+}
---
+++ oops/intro_to_oop.dart
@@ -0,0 +1,47 @@
+void main() {
+  // print("Hello World!");
+
+  // main concepts of oop:
+  // class -> blueprint/model,A Class is a user-defined data type
+  // object ->
+  // inheritance
+  // polymorphism
+  // interfaces
+  // abstract class
+
+  // class
+
+  // class ClassName{
+  //   // field
+  //   // getter/setter function
+  //   // constructor
+  //   // functions
+  // }
+
+  //  Object
+  // An object is a real-life entity such as a table, human, car, etc. The    object has two characteristics - state and behavior.Instances of classes representing real-life entities with state and behavior.
+
+  // var objectName = new ClassName(constructors)
+  // or 
+  // ClassName objectName = new ClassName(constructors)
+
+  // inheritance
+  // Dart supports inheritance, which is used to create new classes from an existing class. The class that to be extended is called parent /superclass, and the newly created class is called child/subclass. Dart provides extends keyword to inherit the properties of parent class in child class. 
+
+  // class ChildClassName extends ParentClassName
+
+  // Polymorphism
+  // Polymorphism is an object-oriented programming concept where one thing has many forms. It can be two types - Runtime polymorphism and Compile time polymorphism. 
+
+  // Interfaces
+  // The interface is defined as a blueprint of the class. We can declare methods and variables inside the interface just like the class but in interface only abstract declaration of methods is provided. We can only define the function signature but not its body. Another class can implement the interface. It is basically used for data-hiding.
+
+  // Abstract Class
+  // A class that contains one or more abstract method is called an abstract class. We can declare the abstract class using the abstract keyword followed by class declaration.
+
+  // abstract class ClassName{
+
+  // }
+
+
+}
